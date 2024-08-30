e6270cb8eeb728e8fea207bbb85d59ccc0a56f8d
Fsenyo197 <Fsenyo197@gmail.com>
2024-08-30 05:23:27 +0000

    Changes

--- pfn/settings.py
+++ pfn/settings.py
@@ -160,7 +160,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_HOST = 'smtp.sendgrid.net'
 # EMAIL_HOST_USER = ''
 # EMAIL_HOST_PASSWORD = ""
-# EMAIL_PORT = 587
+# EMAIL_PORT = 587 
 # EMAIL_USE_TLS = True
 
 # CKEditor Settings
