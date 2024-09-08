3c2cfdcb902534e65296c5c10c775db2ff6020ec
Fsenyo197 <Fsenyo197@gmail.com>
2024-09-08 13:11:53 +0000

    Fix Image field issue

--- blog/models/article_model.py
+++ blog/models/article_model.py
@@ -6,8 +6,6 @@ from tinymce.models import HTMLField
 from blog.utils.blog_utils import count_words, read_time
 from blog.models.category_model import Category
 import cloudinary.uploader
-import cloudinary.api
-from urllib.parse import urlparse
 import environ
 
 # Initialize environment variables
@@ -37,7 +35,7 @@ class Article(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='articles')
     title = models.CharField(max_length=250, null=False, blank=False)
     slug = models.SlugField(unique=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)  # Removed `upload_to='articles/'`
     image_credit = models.CharField(max_length=250, null=True, blank=True)
     body = HTMLField(blank=True)
     tags = TaggableManager(blank=True)
@@ -78,20 +76,11 @@ class Article(models.Model):
                 counter += 1
 
         # Upload image to Cloudinary if a new image is provided
-        if self.image and not self._state.adding:  # Check if image is provided and not a new object
+        if self.image and self._state.adding:  # Upload image when creating a new instance
             upload_response = cloudinary.uploader.upload(self.image)
-            self.image = upload_response.get('secure_url')  # Save the Cloudinary URL
+            self.image = upload_response.get('secure_url')
 
-        # Extract Cloudinary URL from combined URL if needed
-        if self.image:
-            heroku_url = env('HEROKU_APP_URL')  # Load Heroku app URL from environment variable
-            parsed_url = urlparse(self.image.url)
-            if parsed_url.netloc == heroku_url:
-                # Assuming the Cloudinary URL is always after the host URL
-                cloudinary_url = parsed_url.path.lstrip('/')
-                self.image = f'https://{cloudinary_url}'  # Constructing full Cloudinary URL
-
-        # Handle empty body
+        # Handle word count and read time
         self.count_words = count_words(self.body) if self.body else 0
         self.read_time = read_time(self.body) if self.body else 0
 
