55845fa0b2337b0dbfc62bbfe3771898fd751307
Fsenyo197 <Fsenyo197@gmail.com>
2024-09-10 17:00:20 +0000

    fix

--- blog/api/v1/serializers/article_serializers.py
+++ blog/api/v1/serializers/article_serializers.py
@@ -3,7 +3,14 @@ from blog.models.article_model import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
     category = serializers.ReadOnlyField(source="category.name")
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Article
         fields = ('category', 'title', 'slug', 'image', 'body', 'date_published', 'meta_description', 'meta_keywords')
+
+    def get_image(self, obj):
+        if obj.image:
+            # Return only the Cloudinary URL (host URL included)
+            return obj.image.url if hasattr(obj.image, 'url') else obj.image
+        return None
