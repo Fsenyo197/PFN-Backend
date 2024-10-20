fc03c4efd64f6ad2a56dcb90ebdff3ddd513c5a1
Senyo197 <psenyo197@gmail.com>
2024-10-20 20:32:00 +0000

    add discount code

--- blog/api/v1/routers/routers.py
+++ blog/api/v1/routers/routers.py
@@ -4,9 +4,17 @@ from django.urls import path
 # Blog application imports
 from blog.api.v1.views.category_views import CategoryList
 from blog.api.v1.views.article_views import ArticleList, CategoryArticleList
+from blog.api.v1.views.discount_views import DiscountCodeList, ActiveDiscountCodeList, FirmDiscountCodeList, DiscountCodeDetail
 
 urlpatterns = [
+    # Category and Article paths
     path('categories/', CategoryList.as_view(), name='category-list'),
     path('articles/', ArticleList.as_view(), name='article-list'),
     path('articles/category/<str:category_name>/', CategoryArticleList.as_view(), name='category-article-list'),
+
+    # Discount code paths
+    path('discount-codes/', DiscountCodeList.as_view(), name='discount-code-list'),
+    path('discount-codes/active/', ActiveDiscountCodeList.as_view(), name='active-discount-code-list'),
+    path('discount-codes/firm/<str:firm_name>/', FirmDiscountCodeList.as_view(), name='firm-discount-code-list'),
+    path('discount-codes/<str:code>/', DiscountCodeDetail.as_view(), name='discount-code-detail'),
 ]
