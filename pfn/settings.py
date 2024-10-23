a39506fb40ca2e011ca12cf9bd40271b238b20da
Senyo197 <psenyo197@gmail.com>
2024-10-23 11:42:04 +0000

    Delete Discount

--- pfn/settings.py
+++ pfn/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pfn-1b88e7ae7666.herokuapp.com']
 
 # Application definition
 
