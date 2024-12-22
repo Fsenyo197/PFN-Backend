b8b0c56d47ede64b2a3dc5d81f6da72ca05ea48e
Senyo197 <psenyo197@gmail.com>
2024-12-22 17:07:24 +0000

    add prop firm

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django.utils.html import format_html
-from blog.models.propfirm_model import PropFirm
+from blog.models.propfirm_model import PropFirm, AccountPlan
 from blog.models.article_model import Article
 from blog.models.category_model import Category
 from blog.forms.dashboard.propfirm_form import PropFirmForm
-import json
 from blog.models.apikeys_model import APIKey
+
 
 @admin.register(APIKey)
 class APIKeyAdmin(admin.ModelAdmin):
@@ -27,22 +26,50 @@ class APIKeyAdmin(admin.ModelAdmin):
         return actions
 
 
+class AccountPlanInline(admin.TabularInline):
+    model = AccountPlan
+    extra = 1
+    fields = (
+        'phase',
+        'account_size',
+        'price',
+        'profit_split_ratio',
+        'daily_drawdown',
+        'total_drawdown',
+        'currency',
+        'is_available',
+        'status',
+    )
+
+
 @admin.register(PropFirm)
 class PropFirmAdmin(admin.ModelAdmin):
     form = PropFirmForm
+    inlines = [AccountPlanInline]
+
+    list_display = (
+        'name',
+        'website',
+        'year_established',
+        'firm_type',
+        'location',
+        'status',
+        'is_active',
+    )
+    search_fields = ('name', 'location', 'firm_type')
+    list_filter = ('firm_type', 'drawdown_type', 'payout_frequency', 'status', 'date_created', 'date_published', 'is_active')
+    readonly_fields = ( 'date_created', 'date_updated')
+    date_hierarchy = 'date_published'
+    ordering = ['status', '-date_created']
 
     fieldsets = (
         (None, {
-            'fields': ('name', 'year_established', 'firm_type', 'location', 'trading_platforms', 'drawdown_type', 'payout_frequency'),
-        }),
-        ('Account Plans', {
-            'fields': ('account_plans',),
-            'description': format_html(
-                '<div id="account-plans-container"></div>'
-                '<button type="button" id="add-account-plan">Add Account Plan</button>'
+            'fields': (
+                'name', 'website', 'year_established', 'firm_type', 'location',
+                'drawdown_type', 'payout_frequency', 'is_active', 'status',
             ),
         }),
-        ('Trading Rules', {
+        ('Features', {
             'fields': ('news_rule', 'copy_trading', 'two_percent_rule', 'stop_loss_rule', 'consistency_rule'),
         }),
         ('Options', {
@@ -51,36 +78,27 @@ class PropFirmAdmin(admin.ModelAdmin):
         ('Restrictions', {
             'fields': ('countries_prohibited',),
         }),
+        ('Platforms', {
+            'fields': ('trading_platforms',),
+        }),
     )
 
-    def save_model(self, request, obj, form, change):
-        account_plans = []
-        
-        # Retrieve account plan data from POST
-        plan_phases = request.POST.getlist('account_plans[]')
-        plan_sizes = request.POST.getlist('account_size[]')
-        plan_prices = request.POST.getlist('price[]')
-        plan_profit_split_ratio = request.POST.getlist('profit_split_ratio[]')
-        plan_daily_drawdowns = request.POST.getlist('daily_drawdown[]')
-        plan_total_drawdowns = request.POST.getlist('total_drawdown[]')
 
-        # Combine the retrieved data into account plans
-        for i in range(len(plan_phases)):
-            account_plan = {
-                'phase': plan_phases[i],
-                'account_size': plan_sizes[i],
-                'price': plan_prices[i],
-                'profit_split_ratio': plan_profit_split_ratio[i], 
-                'daily_drawdown': plan_daily_drawdowns[i],
-                'total_drawdown': plan_total_drawdowns[i],
-            }
-            account_plans.append(account_plan)
+@admin.register(AccountPlan)
+class AccountPlanAdmin(admin.ModelAdmin):
+    list_display = (
+        'prop_firm',
+        'phase',
+        'account_size',
+        'price',
+        'currency',
+        'is_available',
+        'status',
+    )
+    list_filter = ('phase', 'currency', 'is_available', 'status')
+    search_fields = ('prop_firm__name', 'phase', 'account_size')
+    ordering = ['prop_firm', 'phase', 'account_size']
 
-        obj.account_plans = json.dumps(account_plans)
-        super().save_model(request, obj, form, change)
-
-    class Media:
-        js = ('admin/js/propfirm_admin.js',)
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -90,20 +108,21 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     ordering = ['name']
 
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = (
-        'category', 
-        'title', 
-        'slug', 
-        'status', 
-        'discount_code', 
-        'firm_name', 
-        'discount_percentage', 
-        'duration'
+        'category',
+        'title',
+        'slug',
+        'status',
+        'discount_code',
+        'firm_name',
+        'discount_percentage',
+        'duration',
     )
-    list_filter = ('status', 'date_created', 'date_published',)
-    search_fields = ('title', 'body',)
+    list_filter = ('status', 'date_created', 'date_published')
+    search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
     date_hierarchy = 'date_published'
     ordering = ['status', '-date_created']
@@ -112,13 +131,16 @@ class ArticleAdmin(admin.ModelAdmin):
 
     fieldsets = (
         (None, {
-            'fields': ('category', 'title', 'slug',  'image', 'image_credit', 'body', 'status', 'meta_description', 'meta_keywords', 'date_published', 'views', 'count_words', 'read_time')
-
+            'fields': (
+                'category', 'title', 'slug', 'image', 'image_credit', 'body',
+                'status', 'meta_description', 'meta_keywords', 'date_published',
+                'views', 'count_words', 'read_time',
+            ),
         }),
         ('Discount Information', {
             'fields': (
-                'firm_name', 'discount_code', 'discount_percentage', 
-                'duration', 'website_domain'
+                'firm_name', 'discount_code', 'discount_percentage',
+                'duration', 'website_domain',
             ),
             'classes': ('collapse',),
         }),
--- blog/models/propfirm_model.py
+++ blog/models/propfirm_model.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.db.models import JSONField
+from django.utils import timezone
 
 class PropFirm(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
+
     # Basic Prop Firm Fields
     name = models.CharField(max_length=250, unique=True) 
+    website = models.URLField(null=True, blank=True)
     year_established = models.PositiveIntegerField(null=True, blank=True)
     location = models.TextField(blank=True)
     firm_type = models.CharField(max_length=100, blank=True)
@@ -17,7 +24,7 @@ class PropFirm(models.Model):
     two_percent_rule = models.BooleanField(default=False)
     stop_loss_rule = models.BooleanField(default=False)
     countries_prohibited = models.TextField(blank=True)
-    
+
     # Choices for payout and payment options
     PAYOUT_OPTIONS_CHOICES = [
         ('Bank Transfer', 'Bank Transfer'),
@@ -54,9 +61,6 @@ class PropFirm(models.Model):
         default=list,
     )
 
-    # Account plans as a list of dictionaries
-    account_plans = JSONField(blank=True, default=list)
-
     # Trading platforms field using ArrayField for PostgreSQL
     TRADING_PLATFORMS_CHOICES = [
         ('MT4', 'MT4'),
@@ -72,3 +76,46 @@ class PropFirm(models.Model):
         blank=True,
         default=list,
     )
+
+    # New fields
+    is_active = models.BooleanField(default=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    date_published = models.DateTimeField(null=True, blank=True, default=timezone.now)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AccountPlan(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    ]
+
+    PHASE_CHOICES = [
+        ('one_phase', 'One Phase'),
+        ('two_phase', 'Two Phase'),
+        ('three_phase', 'Three Phase'),
+        ('four_phase', 'Four Phase'),
+        ('instant_funding', 'Instant Funding'),
+    ]
+
+    prop_firm = models.ForeignKey(
+        PropFirm, related_name="account_plans", on_delete=models.CASCADE
+    )
+    phase = models.CharField(max_length=50, choices=PHASE_CHOICES)
+    account_size = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    profit_split_ratio = models.CharField(max_length=50)
+    daily_drawdown = models.CharField(max_length=50)
+    total_drawdown = models.CharField(max_length=50)
+
+    # New fields
+    currency = models.CharField(max_length=10, default="USD")
+    is_available = models.BooleanField(default=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    def __str__(self):
+        return f"{self.phase} - {self.account_size}"
