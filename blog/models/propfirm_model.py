cf76ccd6f2738428be853fd28a296cd4bd813248
Senyo197 <psenyo197@gmail.com>
2025-01-08 08:05:53 +0000

    add slug field

--- blog/models/propfirm_model.py
+++ blog/models/propfirm_model.py
@@ -11,7 +11,7 @@ class PropFirm(models.Model):
 
     # Basic Prop Firm Fields
     name = models.CharField(max_length=250, unique=True)
-    slug = models.SlugField(unique=True, blank=True)
+    slug = models.SlugField(unique=False, blank=True)
     about = models.TextField(blank=True)
     website = models.URLField(null=True, blank=True, unique=True)
     broker = models.CharField(max_length=250, null=True, blank=True)
