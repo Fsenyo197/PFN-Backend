67e3cb98d487344c6a6f8d77041c011dc4bb6e0d
Senyo197 <psenyo197@gmail.com>
2025-01-12 16:01:32 +0000

    final

--- blog/api/v1/views/propfirm_views.py
+++ blog/api/v1/views/propfirm_views.py
@@ -3,15 +3,17 @@ from rest_framework.response import Response
 from blog.api.v1.serializers.propfirm_serializers import PropFirmSerializer
 from blog.models.propfirm_model import PropFirm
 from rest_framework.permissions import IsAuthenticated
-
+from django.db.models import Q
 
 class PropFirmListView(generics.ListAPIView):
-    """List all PropFirms."""
+    """List all PropFirms where status is 'draft' or is_active is False."""
     serializer_class = PropFirmSerializer
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
-        # Optimize the query by prefetching related account plans
-        firms = PropFirm.objects.prefetch_related('account_plans').all()
+        # Use Q to filter by 'status=draft' OR 'is_active=False'
+        firms = PropFirm.objects.prefetch_related('account_plans').filter(
+            Q(status='draft') | Q(is_active=False)
+        )
         serializer = self.get_serializer(firms, many=True)
         return Response(serializer.data)
